0dea339e4c5028693f9870f82aab46f8dccb9787
NikitaSech <NikitaSech@yandex.ru>
2022-10-26 16:14:09 +0300

    Добавил изменение preview в about

--- source/js/modules/theme/theme.js
+++ source/js/modules/theme/theme.js
@@ -4,6 +4,19 @@ export class Theme {
   constructor(pageElement) {
     this.pageElement = pageElement;
     this.buttonElements = pageElement.querySelectorAll('[data-theme-toggler]');
+    this.aboutPreviewElements = pageElement.querySelectorAll('[data-about-preview]');
+  }
+
+  toggleAboutPreview() {
+    this.aboutPreviewElements.forEach((element) => {
+      element.classList.toggle('is-hidden');
+    });
+  }
+
+  toggleButtons() {
+    this.buttonElements.forEach((element) => {
+      toggleButton(element);
+    });
   }
 
   toggle() {
@@ -11,11 +24,13 @@ export class Theme {
   }
 
   buttonClickHundler() {
-    this.buttonElements.forEach((element) => {
-      toggleButton(element);
-    });
+    this.toggleButtons();
+    this.toggleAboutPreview();
     this.toggle();
   }
+
+  // var themeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  // themeQuery.addEventListener('change', setColorScheme(getPreferredColorScheme()));
 
   addListeners() {
     this.buttonElements.forEach((element) => {
